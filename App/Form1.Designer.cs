72284b702551580f30b7f04997f2c593ae3b7dce
VyseMACHINE <40603631+VyseMACHINE@users.noreply.github.com>
2019-08-28 19:27:55 +0600

    Add files via upload

---
+++ App/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace T9App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.writingTB = new System.Windows.Forms.TextBox();
+            this.log = new System.Windows.Forms.TextBox();
+            this.btnAddWord = new System.Windows.Forms.Button();
+            this.btnUseWord = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // writingTB
+            // 
+            this.writingTB.Location = new System.Drawing.Point(12, 12);
+            this.writingTB.Multiline = true;
+            this.writingTB.Name = "writingTB";
+            this.writingTB.Size = new System.Drawing.Size(149, 76);
+            this.writingTB.TabIndex = 0;
+            this.writingTB.TextChanged += new System.EventHandler(this.writingTB_TextChanged);
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(12, 162);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(149, 20);
+            this.log.TabIndex = 1;
+            // 
+            // btnAddWord
+            // 
+            this.btnAddWord.Location = new System.Drawing.Point(12, 103);
+            this.btnAddWord.Name = "btnAddWord";
+            this.btnAddWord.Size = new System.Drawing.Size(149, 23);
+            this.btnAddWord.TabIndex = 2;
+            this.btnAddWord.Text = "Добавить в словарь";
+            this.btnAddWord.UseVisualStyleBackColor = true;
+            this.btnAddWord.Click += new System.EventHandler(this.btnAddWord_Click);
+            // 
+            // btnUseWord
+            // 
+            this.btnUseWord.Location = new System.Drawing.Point(12, 189);
+            this.btnUseWord.Name = "btnUseWord";
+            this.btnUseWord.Size = new System.Drawing.Size(149, 23);
+            this.btnUseWord.TabIndex = 3;
+            this.btnUseWord.Text = "Использовать слово";
+            this.btnUseWord.UseVisualStyleBackColor = true;
+            this.btnUseWord.Click += new System.EventHandler(this.btnUseWord_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(12, 218);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(149, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Выйти";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(176, 248);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnUseWord);
+            this.Controls.Add(this.btnAddWord);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.writingTB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox writingTB;
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.Button btnAddWord;
+        private System.Windows.Forms.Button btnUseWord;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ App/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace T9App
+{
+    public partial class Form1 : Form
+    {
+        List<string> vocabulary = new List<string>();
+        Thread thT9 = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+
+            string vocabularyItemsForLogging = "";
+
+            foreach(var item in vocabulary) vocabularyItemsForLogging += item + "\r\n";
+
+            MessageBox.Show("Стартовый словарь для Т9:\r\n\r\n" + vocabularyItemsForLogging + "\r\n\r\n" + "В программе есть возможность добавления новых слов!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void Init()
+        {
+            vocabulary.Add("Apple");
+            vocabulary.Add("apple");
+            vocabulary.Add("Iphone");
+            vocabulary.Add("iphone");
+            vocabulary.Add("Samsung");
+            vocabulary.Add("samsung");
+            vocabulary.Add("House");
+            vocabulary.Add("house");
+            vocabulary.Add("Computer");
+            vocabulary.Add("computer");
+            vocabulary.Add("Coordinate");
+            vocabulary.Add("coordinate");
+        }
+
+        public string FindLastWord()
+        {
+            int lastSpacePosition = 0;
+
+            string text = writingTB.Text;
+            List<char> symbols = text.ToList<char>();
+ 
+            for(int i = 0; i < symbols.Count; i++)
+            {
+                if (symbols[i] == ' ') lastSpacePosition = (i+1);
+            }
+
+            string result =  text.Remove(0, lastSpacePosition);
+
+            return result;
+        }
+
+        private void writingTB_TextChanged(object sender, EventArgs e)
+        {
+            log.Text = " ";
+            thT9 = new Thread(T9Routine);
+            thT9.IsBackground = true;
+            thT9.Start();
+        }
+
+        private void T9Routine()
+        {
+            log.Invoke(new Action<string>(
+                (x) =>
+                {
+                    log.Text = x;
+                    log.Update();
+                }
+                ), t9(FindLastWord()));
+        }
+
+        public string t9(string str)
+        {
+            string word = "";
+
+            for (int i = 0; i < vocabulary.Count; i++)
+            {
+                if (vocabulary[i].Contains(str))
+                {
+                    word = vocabulary[i];
+                }
+            }
+
+            return word;
+        }
+
+        private void btnAddWord_Click(object sender, EventArgs e)
+        {
+            vocabulary.Add(FindLastWord());
+        }
+
+        private void btnUseWord_Click(object sender, EventArgs e)
+        {
+            string result = " ";
+            if (writingTB.Text.Contains(FindLastWord()))
+            {
+                result = writingTB.Text.Remove(writingTB.Text.Length - FindLastWord().Length, FindLastWord().Length);
+            }
+
+            result += t9(FindLastWord());
+
+            writingTB.Text = result;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
